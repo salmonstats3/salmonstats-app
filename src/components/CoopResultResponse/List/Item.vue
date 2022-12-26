561b5ea06ad49e77b310725fba858d0070ac2aa2
tkgstrator <nasawake.am@gmail.com>
2022-12-26 10:25:32 +0900

    update result list item

--- src/components/CoopResultResponse/List/Item.vue
+++ src/components/CoopResultResponse/List/Item.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="item" :class="result.isClear ? 'is-clear' : 'is-failure'"
+  <div class="coop-result-item" :class="result.isClear ? 'clear' : 'failure'"
     @click.top="$router.push(`results/${result.salmonId}`)">
-    <div class="isClear">
+    <div class="coop-result-job-result" :class="result.isClear ? 'clear' : 'failure'">
       <span>{{ result.isClear ? $t('CoopHistory.Clear') : $t('CoopHistory.Failure') }}</span>
     </div>
-    <div class="playTime">
+    <div class="coop-result-play-time">
       <span>{{ dayjs(result.playTime).format('YYYY MM/DD HH:mm') }}</span>
       <!-- <span>{{ dayjs(result.playTime).fromNow() }}</span> -->
     </div>
-    <div class="ikura">
+    <div class="coop-result-ikura">
       <div>
-        <img src="@/assets/images/gold-ikura.542af9c3faf6cd81f558fb11d2206995.svg" alt="gold-ikura">
-        <span>{{ result.goldenIkuraNum }}</span>
+        <img src="@/assets/images/gold-ikura.542af9c3faf6cd81f558fb11d2206995.svg" alt="golden-ikura">
+        <span class="num">{{ result.goldenIkuraNum }}</span>
       </div>
       <div>
         <img src="@/assets/images/ikura.b84a6213dc0a5053e99b63d9be7dd4d3.svg" alt="ikura">
-        <span>{{ result.ikuraNum }}</span>
+        <span class="num">{{ result.ikuraNum }}</span>
       </div>
     </div>
   </div>
@@ -30,41 +30,57 @@ const { result } = defineProps<Props>()
 </script>
 
 <style scoped lang="scss">
-.item {
-  border-bottom: 1px solid rgb(30, 30, 30);
+.coop-result-item {
   padding: 12px 18px;
   display: flex;
   align-items: center;
-  &.is-clear {
+  color: azure;
+
+  &.clear {
     background-color: rgb(210, 100, 0);
-    color: rgb(250, 250, 250);
   }
-  &.is-failure {
+
+  &.failure {
     background-color: rgb(160, 100, 0);
   }
-  &:hover, &:active {
+
+  &:hover,
+  &:active {
     cursor: pointer;
     background-color: rgb(130, 100, 0);
   }
 }
 
-.isClear {
+.coop-result-job-result {
+  font-family: var(--font-family-s1);
   font-size: 16px;
   margin-right: 12px;
+
+  &.clear {
+    color: var(--color-yellow);
+  }
 }
 
-.playTime {
+.coop-result-play-time {
+  font-family: var(--font-family-s2);
   font-size: 12px;
 }
 
-.ikura {
+.coop-result-ikura {
+  font-family: var(--font-family-s2);
   font-size: 16px;
   margin-left: auto;
   margin-right: 0;
   text-align: right;
+
   img {
     margin-right: 4px;
     height: 14px;
   }
+}
+
+.num::before {
+  content: "x";
+  padding-right: 2px;
 }
 </style>
