475879a47912da91a2f566b04fe966076f1395c4
tkgstrator <nasawake.am@gmail.com>
2022-12-27 22:43:44 +0900

    fix: component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,9 @@
 <script setup lang="ts">
 import ResultsView from "@/components/tabs/results.vue"
 </script>
+
 <template>
-  <div class="page">
-    <div class="loading" v-if="pending">Loading ...</div>
-    <div v-else>
-      <div class="coop-result-list">
-        <CoopResultResponseListItem v-for="result in results" :key="`result-${result.salmonId}`" :result="result" />
-      </div>
-      <InfiniteLoading @infinite="loadData" class="inifinite-loading" />
-    </div>
-  </div>
+  <ResultsView />
 </template>
 
 <template>
